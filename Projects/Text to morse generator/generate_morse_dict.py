5dfe830c6387a018d01952d36146b193102c328c
Lateef <lateefwithcodes@gmail.com>
2022-09-10 12:04:25 +0100

    Initial commit

---
+++ Projects/Text to morse generator/generate_morse_dict.py
@@ -0,0 +1,62 @@
+alphabet_morse = {
+    "A": ". -",
+    "B": "",
+    "C": "",
+    "D": "",
+    "E": "",
+    "F": "",
+    "G": "",
+    "H": "",
+    "I": "",
+    "J": "",
+    "K": "",
+    "L": "",
+    "M": "",
+    "N": "",
+    "O": "",
+    "P": "",
+    "Q": "",
+    "R": "",
+    "S": "",
+    "T": "",
+    "U": "",
+    "V": "",
+    "W": "",
+    "X": "",
+    "Y": "",
+    "Z": "",
+    "0": "",
+    "1": "",
+    "2": "",
+    "3": "",
+    "4": "",
+    "5": "",
+    "6": "",
+    "7": "",
+    "8": "",
+    "9": "",
+}
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.wildwalks.com/bushcraft/technical-stuff/morse-code-and-phonetic-alphabet.html"
+
+response = requests.get(url=url)
+soup = BeautifulSoup(response.text, "html.parser")
+letters_raw = soup.find_all(name="td", class_="xl26")
+morses_raw = soup.find_all(name="td", class_="xl27")
+letters = []
+morses = []
+for letter in letters_raw:
+    letters.append(letter.getText())
+
+for morse in morses_raw:
+    stripped_morse = "".join(morse.getText().split(" "))
+    morses.append(stripped_morse)
+
+
+letter_morse = {}
+for i in range(len(letters)):
+    letter_morse[letters[i]] = morses[i]
+
+print(letter_morse)
